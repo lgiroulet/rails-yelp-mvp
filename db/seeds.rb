6766d659396957426f688d80144ab020a9210a6c
Lea Giroulet <lgiroulet@hotmail.fr>
2020-11-11 18:13:25 +0100

    rails yelp app completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bristol = Restaurant.create(name: 'Epicure', category: 'french', address: '75008 Paris', phone_number: '0134563461')
+sushis = Restaurant.create(name: 'Saigon', category: 'japanese', address: '75010 Paris', phone_number: '0134503461')
+fricadelle = Restaurant.create(name: 'fricadelle', category: 'belgian', address: '75008 Paris', phone_number: '0134533461')
+italian = Restaurant.create(name: 'aldente', category: 'italian', address: '75016 Paris', phone_number: '0147563461')
+hype = Restaurant.create(name: 'BuddhaBar', category: 'japanese', address: '75008 Paris', phone_number: '0134563461')
+
+
